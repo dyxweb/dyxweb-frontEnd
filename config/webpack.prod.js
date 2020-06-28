2bec29bc3b2957b8ca061002259b736cf1c9585e
dyxweb <13483409091@163.com>
2020-06-28 20:56:40 +0800

    feat: 压缩gzip形式

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,6 +3,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const CompressionPlugin = require('compression-webpack-plugin');
 const smp = new SpeedMeasurePlugin();
 
 module.exports = merge(common, {
@@ -22,5 +23,14 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       'QUERYHOST': JSON.stringify('http://47.99.184.72:7001/api'),
     }), // 定义全局的变量(一般用于区分生产环境和开发环境)
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip', // 算法
+      test: new RegExp(
+        '\\.(js|css|less|scss)$' // 压缩 js 与 css
+      ),
+      // threshold: 10240,//只处理比这个值大的资源。按字节计算
+      // minRatio: 0.8//只有压缩率比这个值小的资源才会被处理
+    }),
   ]
 })
